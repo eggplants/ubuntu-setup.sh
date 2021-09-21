b4caca60477fb846380bb7e37418ced43d908936
haruna <w10776e8w@yahoo.co.jp>
2021-09-21 21:20:10 +0900

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -53,6 +53,14 @@ wait_enter install useful commands && (
   sudo snap install yq nkf
 )
 
+wait_enter install gh && (
+  cmd_exist gh && exit
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  sudo apt update
+  sudo apt install gh -y
+)
+
 wait_enter install and configure japanese input && (
   sudo apt install ibus-mozc -y
   ibus restart
