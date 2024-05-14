74fc80a093310a32e118a0c29818b4c219b73061
haruna <w10776e8w@yahoo.co.jp>
2024-05-15 00:06:37 +0900

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -21,7 +21,9 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y \
   byobu curl feh ffmpeg git \
-  ibus-mozc imagemagick jq pinentry-tty pkg-config \
+  ibus-mozc imagemagick jq \
+  network-manager-l2tp network-manager-l2tp-gnome \
+  pinentry-tty pkg-config \
   rhythmbox unar w3m wget zsh
 
 # for python + ruby
